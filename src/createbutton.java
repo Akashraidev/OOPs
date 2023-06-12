6119d8f92e80d460cb2df9b226fda66055fafe10
akashraidev <akashrai3334@gmail.com>
2023-06-12 13:43:07 +0530

    buttonAWT

---
+++ src/createbutton.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+import static java.awt.Color.red;
+
+public class createbutton extends Frame {
+    public createbutton()
+    {
+        this.setBackground(Color.cyan);
+        Button b =new Button("Click here");
+        b.setBounds(5,1,50,22);
+        add(b);
+        this.setVisible(true);
+        this.setSize(600,600);
+        this.setTitle("create button");
+    }
+    public static void main(String[] args) {
+        createbutton cb= new createbutton();
+    }
+}
