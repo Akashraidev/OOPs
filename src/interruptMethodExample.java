7c902b57c625d790724285180788a269cd4917ca
AKASH RAI <120483379+Akashraidev@users.noreply.github.com>
2023-06-27 00:09:19 +0530

    Add files via upload

---
+++ src/interruptMethodExample.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+public class interruptMethodExample extends Thread{
+    public void run()
+    {
+        try
+        {
+            for (int j=1;j<=5;j++)
+            {
+                System.out.println(j);
+                Thread.sleep(1000);
+            }
+        }catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        interruptMethodExample i = new interruptMethodExample();
+        i.start();
+        i.interrupt();
+    }
+}
